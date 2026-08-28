55de391d73d729dd905de8967333b1d9ad83cf64
28GEngle <28GEngle@live.ytech.edu>
2026-08-28 14:55:50 -0400

    Fixed the HTML sending issue!
    Instead of using async which I don't entirely know how to do yet, I opted to use synchronous file reading, and save it  to a variable, instead of reading every time the page is loaded and not saving it.

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 const http = require("http");
 const fs = require("fs");
 
-async function readFile(filePath) {
-    fs.readFile(filePath, "utf8", function (err, data) {
-        promise = Promise.resolve(String(data));
-        promise.then((value) => {
-            return value
-        })
-    });
-}
-
-const server = http.createServer(function(req, res) {
-    res.writeHead(200);
-    try {
-        res.end(readFile("home.html"))
-    } catch (error) {
-        console.log(readFile("home.html"))
-    }
+contents = fs.readFileSync("home.html", "utf8", function (err, data) {
+    return data;
 });
 
-server.listen(3000,"127.0.0.1");
+const server = http.createServer(function (req, res) {
+    res.writeHead(200);
+    res.end(contents);
+});
+
+server.listen(3000, "127.0.0.1");
